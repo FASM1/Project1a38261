46aea08abeb350febbc554b7bbb5703db1afc25f
FASM1 <filipeari@live.com.pt>
2024-05-01 17:39:39 +0100

    1.Allow the vehicle to move left/right
    a38261@1.Allow the vehicle to move left/right

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,6 @@ public class FollowPlayer : MonoBehaviour
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
-    
+        
     }
 }
--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float speed = 5.0f;
-
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +17,6 @@ public class PlayerControler : MonoBehaviour
     {
         //we'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
