862ddd27e66ae09768bf8d817dc1a7852858cde1
FASM1 <filipeari@live.com.pt>
2024-06-24 18:21:01 +0100

    1.Make the vehicle use forces
    1.Make the vehicle use forces@38261

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
-    [SerializeField] float speed = 20.0f;
+    [SerializeField] private float horsePower = 0;
+    //[SerializeField] float speed = 20.0f;
     [SerializeField] float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
-    
+    private Rigidbody playerRb;
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         //Moves the car forward based on vertical inputt
-        transform.Translate(Vector3.forward * Time.deltaTime * speed *forwardInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed *forwardInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
