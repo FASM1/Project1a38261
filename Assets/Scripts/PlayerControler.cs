b286b5cdc4dbe12f92fd6eb87dc7379295e72fc7
FASM1 <filipeari@live.com.pt>
2024-06-24 19:37:59 +0100

    5.Prevent driving in mid-air
    5.Prevent driving in mid-air@38261

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,36 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using TMPro;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] float speed;
+    [SerializeField] float rpm;
     [SerializeField] private float horsePower = 0;
-    //[SerializeField] float speed = 20.0f;
-    [SerializeField] float turnSpeed = 45.0f;
+    [SerializeField] float turnSpeed = 30.0f;
     private float horizontalInput;
     private float forwardInput;
     private Rigidbody playerRb;
-    //[SerializeField] GameObject centerOfMass;
+
     [SerializeField] TextMeshProUGUI speedometerText;
-    [SerializeField] float speed;
+    [SerializeField] TextMeshProUGUI rpmText;
+
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
+
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        //playerRb.centerOfMass = centerOfMass.transform.position;
+        
     }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Get Input from player
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        //Moves the car forward based on vertical inputt
-        //transform.Translate(Vector3.forward * Time.deltaTime * speed *forwardInput);
-        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
-        // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
-        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
-        speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f);// 3.6 for kph
-        speedometerText.SetText("Speed" + speed + "kmh");
+
+        if (IsOnGround())
+        {
+
+            //Move the vehicle forward
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            playerRb.AddRelativeForce (UnityEngine.Vector3.forward * forwardInput * horsePower);
+            //Turning the vehicle
+            transform.Rotate (UnityEngine.Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+            //print speed
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 2.237f);
+            speedometerText.SetText("Speed: " + speed + " mph");
+
+            //print RPM
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
